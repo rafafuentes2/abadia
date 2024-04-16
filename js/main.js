3cd490a513e63cd1d6a65d48460fe588039e24a9
rafafuentes2 <rafafuentes_2@hotmail.com>
2024-04-15 22:21:32 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,16 +22,18 @@ if (nombreProveedor && valorProveedor){
 function calculoCierre(){
     let calculoTotal;
     let totalProv = 0;
-    let totalBase = getElementsByClassName("totalBase")[0].value
-    let totalMon = getElementsByClassName("totalMon")[0].value
-    let totalBill = getElementsByClassName("totalBill")[0].value
-    let totalTrans = getElementsByClassName("totalTrans")[0].value
-    let totalVentasDia = getElementsByClassName("totalVentasDia")[0].value
+    let totalBase = document.getElementsByClassName("totalBase")[0].value
+    let totalMon = document.getElementsByClassName("totalMon")[0].value
+    let totalBill = document.getElementsByClassName("totalBill")[0].value
+    let totalTrans = document.getElementsByClassName("totalTrans")[0].value
+    let totalVentasDia = document.getElementsByClassName("totalVentasDia")[0].value
     
     proveedoresArr.forEach (function(numero){
         totalProv += Number(numero.valorProveedor);
     });
     calculoTotal =  Number(totalBill) + Number(totalTrans) + Number(totalMon) + Number(totalBase);
     calculoTotal = calculoTotal - totalProv;
-    
+    calculoTotal = calculoTotal - totalVentasDia;
+    document.getElementsByClassName("resultado")[0].innerHTML = "$" + new Intl.NumberFormat('en-DE').format(calculoTotal)
+
 }
